ede7d6dd4aabf6f5c893d304c9e59ebd7c5ecc58
andots <18536+andots@users.noreply.github.com>
2025-02-14 08:49:11 +0900

    refactor: actual window creation move to the end

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -36,41 +36,19 @@ impl Default for WindowGeometry {
 }
 
 pub fn create_window(app_handle: &tauri::AppHandle, settings: &UserSettings) -> tauri::Result<()> {
-    // Create an initial webview window with the given options
-    let title = format!(
-        "{} - v{}",
-        app_handle.package_info().name,
-        app_handle.package_info().version
-    );
-    let geometry = app_handle.load_window_geometry();
-    let width = geometry.width;
-    let height = geometry.height;
-    let x = geometry.x;
-    let y = geometry.y;
-    let sidebar_width = geometry.sidebar_width;
-    let header_height = geometry.header_height;
-
-    let window = WindowBuilder::new(app_handle, MAINWINDOW_LABEL)
-        .resizable(true)
-        .fullscreen(false)
-        .title(title)
-        .position(x, y)
-        .inner_size(width, height)
-        .build()?;
-
-    // webview for app
+    // App webview
     let mut app_webview_builder = WebviewBuilder::<tauri::Wry>::new(
         APP_WEBVIEW_LABEL,
         WebviewUrl::App(APP_WEBVIEW_URL.into()),
     )
     .auto_resize();
 
+    // External webview
     let url = match Url::parse(&settings.start_page_url) {
         Ok(url) => url,
         Err(_) => Url::parse(default_start_page_url().as_str()).unwrap(),
     };
 
-    // external webview overlayed app_webview
     let handle = app_handle.clone();
     let mut external_webview_builder =
         WebviewBuilder::<tauri::Wry>::new(EXTERNAL_WEBVIEW_LABEL, WebviewUrl::External(url))
@@ -110,12 +88,35 @@ pub fn create_window(app_handle: &tauri::AppHandle, settings: &UserSettings) -> 
     app_webview_builder = app_webview_builder.incognito(incognito);
     external_webview_builder = external_webview_builder.incognito(incognito);
 
+    // Create main window and add webviews, load geometry from file
+    let title = format!(
+        "{} - v{}",
+        app_handle.package_info().name,
+        app_handle.package_info().version
+    );
+    let geometry = app_handle.load_window_geometry();
+    let width = geometry.width;
+    let height = geometry.height;
+    let x = geometry.x;
+    let y = geometry.y;
+    let sidebar_width = geometry.sidebar_width;
+    let header_height = geometry.header_height;
+
+    let window = WindowBuilder::new(app_handle, MAINWINDOW_LABEL)
+        .resizable(true)
+        .fullscreen(false)
+        .title(title)
+        .position(x, y)
+        .inner_size(width, height)
+        .build()?;
+
     let _app_webview = window.add_child(
         app_webview_builder,
         LogicalPosition::new(0., 0.),
         LogicalSize::new(width, height),
     )?;
 
+    // External webview is overlaid on top of the app webview
     let _external_webview = window.add_child(
         external_webview_builder,
         LogicalPosition::new(sidebar_width, header_height),
