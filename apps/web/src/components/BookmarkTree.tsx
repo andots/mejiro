def8e47f6f9345e900f745bbede4b50e6e59f44c
andots <18536+andots@users.noreply.github.com>
2025-02-14 01:16:16 +0900

    feat: enhance BookmarkTree component with folder and bookmark icons

--- apps/web/src/components/BookmarkTree.tsx
+++ apps/web/src/components/BookmarkTree.tsx
@@ -1,4 +1,4 @@
-import { type Component, For, Show, createSignal } from "solid-js";
+import { type Component, For, Match, Show, Switch, createSignal } from "solid-js";
 
 import { useBookmarkState } from "../stores/bookmarks";
 import { useUrlState } from "../stores/url";
@@ -25,7 +25,7 @@ type BookmarkNodeProps = {
 const BookmarkNode: Component<BookmarkNodeProps> = (props) => {
   const navigateToUrl = useUrlState((state) => state.navigateToUrl);
 
-  const [isOpen, setIsOpen] = createSignal(props.level < 2);
+  const [isOpen, setIsOpen] = createSignal(true);
   const hasChildren = () => props.bookmark.children?.length > 0;
 
   const toggleFolder = (e: MouseEvent) => {
@@ -44,25 +44,38 @@ const BookmarkNode: Component<BookmarkNodeProps> = (props) => {
     <li>
       <div
         class={
-          "flex items-center text-left text-sm pb-1 hover:bg-gray-100 cursor-pointer transition-colors duration-150"
+          "flex items-center text-left text-sm pb-2 hover:bg-sidebar-accent cursor-pointer transition-colors duration-150"
         }
         onClick={toggleFolder}
         onKeyDown={handleKeydown}
         style={{ "padding-left": `${props.level}px` }}
       >
-        <span class="w-5 h-5 flex items-center justify-center mr-1.5 text-gray-500">
-          {props.bookmark.node_type === "Folder" || props.bookmark.node_type === "Root" ? (
-            <FolderIcon isOpen={isOpen()} />
-          ) : (
-            <Favicon url={`https://${props.bookmark.host}`} width="17" height="17" />
-          )}
+        {/* Folder icon or Favicon */}
+        <span class="flex items-center justify-center mr-1">
+          <Switch>
+            <Match
+              when={props.bookmark.node_type === "Folder" || props.bookmark.node_type === "Root"}
+            >
+              <Arrow isOpen={isOpen()} />
+              <Folder isOpen={isOpen()} />
+            </Match>
+            <Match when={props.bookmark.node_type === "Bookmark" && hasChildren()}>
+              <Arrow isOpen={isOpen()} />
+              <Favicon url={`https://${props.bookmark.host}`} width="17" height="17" />
+            </Match>
+            <Match when={props.bookmark.node_type === "Bookmark" && !hasChildren()}>
+              <span class="pl-3 pr-1">-</span>
+            </Match>
+          </Switch>
         </span>
         {props.bookmark.node_type === "Bookmark" ? (
-          <span class="p-0.5 text-blue-500 overflow-hidden whitespace-nowrap text-ellipsis">
+          <span class="text-blue-500 overflow-hidden whitespace-nowrap text-ellipsis">
             {props.bookmark.title}
           </span>
         ) : (
-          <span class="text-gray-700 py-0.5">{props.bookmark.title}</span>
+          <span class="text-sidebar-foreground overflow-hidden whitespace-nowrap text-ellipsis">
+            {props.bookmark.title}
+          </span>
         )}
       </div>
       <Show when={hasChildren() && isOpen()}>
@@ -76,23 +89,34 @@ const BookmarkNode: Component<BookmarkNodeProps> = (props) => {
   );
 };
 
-const FolderIcon = (props: { isOpen: boolean }) => (
-  <svg class="w-4 h-4" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+// https://icon-sets.iconify.design/ic/page-2.html?icon-filter=folder
+const Folder = (props: { isOpen: boolean }) => (
+  <svg class="w-4 h-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <title>folder</title>
     {props.isOpen ? (
-      <path d="M1.5 3A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3h-13z" />
+      <path
+        fill="currentColor"
+        d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2m0 12H4V8h16z"
+      />
     ) : (
-      <path d="M.5 3A1.5 1.5 0 0 0-1 4.5v7A1.5 1.5 0 0 0 .5 13h13a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 13.5 3h-13z" />
+      <path
+        fill="currentColor"
+        d="m9.17 6l2 2H20v10H4V6zM10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8z"
+      />
+    )}
+  </svg>
+);
+
+// https://icon-sets.iconify.design/ic/page-2.html?icon-filter=arrow&prefixes=Baseline
+const Arrow = (props: { isOpen: boolean }) => (
+  <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <title>ic:baseline-keyboard-arrow-right</title>
+    {props.isOpen ? (
+      <path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z" />
+    ) : (
+      <path fill="currentColor" d="M8.59 16.59L13.17 12L8.59 7.41L10 6l6 6l-6 6z" />
     )}
   </svg>
 );
 
 export default BookmarkTree;
-
-// const LinkIcon = () => (
-//   <svg class="w-4 h-4" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-//     <title>link</title>
-//     <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
-//     <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z" />
-//   </svg>
-// );
