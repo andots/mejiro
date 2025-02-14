426f065a7800eb6f51de1b9182af6f2c246db400
andots <18536+andots@users.noreply.github.com>
2025-02-14 22:53:39 +0900

    refactor: clean up unused SVG code and add license comment for Google Material Icons

--- apps/web/src/components/icons/Icons.tsx
+++ apps/web/src/components/icons/Icons.tsx
@@ -1,5 +1,6 @@
 import type { JSX } from "solid-js";
 
+// Google Material Icons, Apatch 2.0
 // https://icones.js.org/collection/ic?variant=Baseline
 
 export function IcBaselineRemoveCircleOutline(props: JSX.IntrinsicElements["svg"]) {
@@ -105,17 +106,3 @@ export function IcBaselineStarBorder(props: JSX.IntrinsicElements["svg"]) {
     </svg>
   );
 }
-// <svg
-//   class={`w-4 h-4 ${isFavorited() ? "text-yellow-500 fill-current" : "text-gray-600"}`}
-//   fill="none"
-//   stroke="currentColor"
-//   viewBox="0 0 24 24"
-// >
-//   <title>icon</title>
-//   <path
-//     stroke-linecap="round"
-//     stroke-linejoin="round"
-//     stroke-width="2"
-//     d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-//   />
-// </svg>
