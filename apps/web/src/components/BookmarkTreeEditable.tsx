db26997cea24ed58060d7b41661b061dd95eb9df
andots <18536+andots@users.noreply.github.com>
2025-02-16 14:53:39 +0900

    feat: wrap bookmark item with ContextMenu

--- apps/web/src/components/BookmarkTreeEditable.tsx
+++ apps/web/src/components/BookmarkTreeEditable.tsx
@@ -13,6 +13,24 @@ import { useBookmarkState } from "../stores/bookmarks";
 import type { Bookmark } from "../types";
 import Favicon from "./icons/Favicon";
 
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuGroup,
+  ContextMenuGroupLabel,
+  ContextMenuItem,
+  ContextMenuPortal,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@repo/ui/context-menu";
+
 const BookmarkTreeEditable: Component = () => {
   const bookmarks = useBookmarkState((state) => state.bookmarks);
 
@@ -54,77 +72,108 @@ const BookmarkNode: Component<BookmarkNodeProps> = (props) => {
 
   return (
     <li>
-      <div
-        class={
-          "flex items-center text-left text-sm py-1 hover:bg-sidebar-accent transition-colors duration-150"
-        }
-        onClick={toggle}
-        onKeyDown={handleKeydown}
-        style={{ "padding-left": `${props.level}px` }}
-      >
-        {/* Folder icon or Favicon */}
-        <span class="flex items-center justify-center mr-1">
-          <Switch>
-            <Match
-              when={props.bookmark.node_type === "Folder" || props.bookmark.node_type === "Root"}
-            >
-              <span class="flex w-[40px]">
-                <Show when={isOpen()}>
-                  <IcBaselineKeyboardArrowDown width={20} height={20} />
-                  <IcOutlineFolderOpen width={20} height={20} />
-                </Show>
-                <Show when={!isOpen()}>
-                  <IcBaselineKeyboardArrowRight width={20} height={20} />
-                  <IcOutlineFolder width={20} height={20} />
-                </Show>
-              </span>
-            </Match>
-            <Match when={props.bookmark.node_type === "Bookmark" && hasChildren()}>
-              <span class="flex w-[40px]">
-                <Show when={isOpen()}>
-                  <IcBaselineKeyboardArrowDown width={20} height={20} />
-                </Show>
-                <Show when={!isOpen()}>
-                  <IcBaselineKeyboardArrowRight width={20} height={20} />
-                </Show>
-                <Favicon url={`https://${props.bookmark.host}`} width="18" height="18" />
-              </span>
-            </Match>
-            <Match when={props.bookmark.node_type === "Bookmark" && !hasChildren()}>
-              <span class="flex w-[24px] ml-[20px]">
-                <Favicon url={`https://${props.bookmark.host}`} width="18" height="18" />
-              </span>
-            </Match>
-          </Switch>
-        </span>
-
-        {/* Title */}
-        <span class="text-sidebar-foreground overflow-hidden whitespace-nowrap text-ellipsis">
-          {props.bookmark.title}
-        </span>
-
-        <Show when={props.bookmark.node_type !== "Root"}>
-          {/* Edit button */}
-          <Button
-            class="w-8 h-8 ml-3"
-            variant="outline"
-            size="icon"
-            onClick={() => handleEdit(props.bookmark.index)}
+      <ContextMenu>
+        <ContextMenuTrigger class="">
+          <div
+            class={
+              "flex items-center text-left text-sm py-1 hover:bg-sidebar-accent transition-colors duration-150"
+            }
+            onClick={toggle}
+            onKeyDown={handleKeydown}
+            style={{ "padding-left": `${props.level}px` }}
           >
-            <IcBaselineEdit />
-          </Button>
+            {/* Folder icon or Favicon */}
+            <span class="flex items-center justify-center mr-1">
+              <Switch>
+                <Match
+                  when={
+                    props.bookmark.node_type === "Folder" || props.bookmark.node_type === "Root"
+                  }
+                >
+                  <span class="flex w-[40px]">
+                    <Show when={isOpen()}>
+                      <IcBaselineKeyboardArrowDown width={20} height={20} />
+                      <IcOutlineFolderOpen width={20} height={20} />
+                    </Show>
+                    <Show when={!isOpen()}>
+                      <IcBaselineKeyboardArrowRight width={20} height={20} />
+                      <IcOutlineFolder width={20} height={20} />
+                    </Show>
+                  </span>
+                </Match>
+                <Match when={props.bookmark.node_type === "Bookmark" && hasChildren()}>
+                  <span class="flex w-[40px]">
+                    <Show when={isOpen()}>
+                      <IcBaselineKeyboardArrowDown width={20} height={20} />
+                    </Show>
+                    <Show when={!isOpen()}>
+                      <IcBaselineKeyboardArrowRight width={20} height={20} />
+                    </Show>
+                    <Favicon url={`https://${props.bookmark.host}`} width="18" height="18" />
+                  </span>
+                </Match>
+                <Match when={props.bookmark.node_type === "Bookmark" && !hasChildren()}>
+                  <span class="flex w-[24px] ml-[20px]">
+                    <Favicon url={`https://${props.bookmark.host}`} width="18" height="18" />
+                  </span>
+                </Match>
+              </Switch>
+            </span>
 
-          {/* Remove button */}
-          <Button
-            class="w-8 h-8 ml-3"
-            variant="outline"
-            size="icon"
-            onClick={() => handleRemove(props.bookmark.index)}
-          >
-            <IcOutlineDeleteOutline />
-          </Button>
-        </Show>
-      </div>
+            {/* Title */}
+            {/* <BookmarkContextMenu bookmark={props.bookmark} /> */}
+
+            <span class="text-sidebar-foreground overflow-hidden whitespace-nowrap text-ellipsis">
+              {props.bookmark.title}
+            </span>
+
+            <Show when={props.bookmark.node_type !== "Root"}>
+              {/* Edit button */}
+              <Button
+                class="w-8 h-8 ml-3"
+                variant="outline"
+                size="icon"
+                onClick={() => handleEdit(props.bookmark.index)}
+              >
+                <IcBaselineEdit />
+              </Button>
+
+              {/* Remove button */}
+              <Button
+                class="w-8 h-8 ml-3"
+                variant="outline"
+                size="icon"
+                onClick={() => handleRemove(props.bookmark.index)}
+              >
+                <IcOutlineDeleteOutline />
+              </Button>
+            </Show>
+          </div>
+        </ContextMenuTrigger>
+        <ContextMenuPortal>
+          <ContextMenuContent class="w-48">
+            <ContextMenuItem>
+              <span>Edit</span>
+            </ContextMenuItem>
+
+            <ContextMenuSeparator />
+
+            <ContextMenuItem>
+              <span>Add Folder</span>
+            </ContextMenuItem>
+            <ContextMenuItem>
+              <span>Add Bookmark</span>
+            </ContextMenuItem>
+
+            <ContextMenuSeparator />
+
+            <ContextMenuItem>
+              <span class="text-destructive">Delete</span>
+            </ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenuPortal>
+      </ContextMenu>
+
       <Show when={hasChildren() && isOpen()}>
         <ul class="ml-2">
           <For each={props.bookmark.children}>
