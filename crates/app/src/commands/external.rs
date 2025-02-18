3c9d03ec9933940d268f948751829a395421d1c5
andots <18536+andots@users.noreply.github.com>
2025-02-18 17:35:26 +0900

    feat: update window title with external page title

--- crates/app/src/commands/external.rs
+++ crates/app/src/commands/external.rs
@@ -1,8 +1,18 @@
-use crate::events::{emit_to_app_webview, AppEvent};
+use tauri::Manager;
+
+use crate::{
+    app_handle_ext::AppHandleExt,
+    constants::MAINWINDOW_LABEL,
+    events::{emit_to_app_webview, AppEvent},
+};
 
 #[tauri::command]
 pub fn send_page_title(app_handle: tauri::AppHandle, title: String) {
-    let _ = emit_to_app_webview(&app_handle, AppEvent::ExternalTitleChanged, title);
+    let _ = emit_to_app_webview(&app_handle, AppEvent::ExternalTitleChanged, title.clone());
+    if let Some(window) = app_handle.get_window(MAINWINDOW_LABEL) {
+        let new_title = format!("{} | {}", title, app_handle.get_default_app_title());
+        let _ = window.set_title(&new_title);
+    }
 }
 
 #[tauri::command]
