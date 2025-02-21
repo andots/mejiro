8b21065601a21e5e837c98a2f183eff6a7da3103
andots <18536+andots@users.noreply.github.com>
2025-02-21 18:12:22 +0900

    refactor: update file name serialization for clarity in debug and release builds

--- crates/app/src/app_handle_ext.rs
+++ crates/app/src/app_handle_ext.rs
@@ -11,20 +11,21 @@ use crate::{
 };
 
 /// The file names are defined as an enum to prevent typos and to provide a centralized list of all data files.
-/// Bookmarks and window geometry data are stored in JSON format, but with a dot prefix and no extension name
-/// to prevent accidental deletion or modification by users.
-/// Different file names are used for debug and release builds to separate development and production data.
+/// Different file names should be used for debug and release builds to separate development and production data.
 #[cfg(debug_assertions)]
 #[derive(AsRefStr)]
 pub enum FileName {
-    #[strum(serialize = ".dev-bookmarks")]
+    #[strum(serialize = "dev-bookmarks.json")]
     Bookmarks,
-    #[strum(serialize = ".dev-window_geometry")]
+    #[strum(serialize = "dev-window_geometry.json")]
     WindowGeometry,
     #[strum(serialize = "dev-settings.json")]
     Settings,
 }
 
+/// File names for release builds.
+/// Bookmarks and window geometry data are stored in JSON format, but with a dot prefix and no extension name
+/// to prevent accidental deletion or modification by users.
 #[cfg(not(debug_assertions))]
 #[derive(AsRefStr)]
 pub enum FileName {
