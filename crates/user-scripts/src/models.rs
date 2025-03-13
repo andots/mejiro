58e69bf48195efc8a291ba515c82d9045278c2cd
andots <18536+andots@users.noreply.github.com>
2025-03-13 12:59:48 +0900

    feat(user-scripts): parse metadata of user script

---
+++ crates/user-scripts/src/models.rs
@@ -0,0 +1,111 @@
+#![allow(unused)]
+
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+
+    #[error("No filename")]
+    NoFileName(),
+
+    #[error("No @name")]
+    NoName(),
+}
+
+#[derive(Debug)]
+struct Data {
+    key: String,
+    value: String,
+}
+
+impl Data {
+    pub fn new(key: impl Into<String>, value: impl Into<String>) -> Self {
+        Self {
+            key: key.into(),
+            value: value.into(),
+        }
+    }
+}
+
+type Metadata = Vec<Data>;
+
+trait Finder {
+    fn get(&self, key: &str) -> Option<String>;
+    fn has_key(&self, key: &str) -> bool;
+    fn find_all(&self, key: &str) -> Vec<String>;
+}
+
+impl Finder for Metadata {
+    fn get(&self, key: &str) -> Option<String> {
+        self.iter().find(|d| d.key == key).map(|d| d.value.clone())
+    }
+    fn has_key(&self, key: &str) -> bool {
+        self.iter().any(|d| d.key == key)
+    }
+    fn find_all(&self, key: &str) -> Vec<String> {
+        self.iter()
+            .filter(|d| d.key == key)
+            .map(|d| d.value.clone())
+            .collect::<Vec<_>>()
+    }
+}
+
+#[derive(Debug)]
+pub struct UserScript {
+    script: String,
+    name: Option<String>,
+    description: Option<String>,
+    version: Option<String>,
+    author: Option<String>,
+    match_patterns: Vec<String>,
+}
+
+static METADATA_KEY_VALUE_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"@(?<key>[\w-]+)\s+(?<value>.+)").unwrap());
+
+impl UserScript {
+    pub fn parse(script: &str) -> Self {
+        let mut metadata = vec![];
+        let meta_block = script
+            .lines()
+            .skip_while(|line| !line.trim().starts_with("// ==UserScript=="))
+            .skip(1)
+            .take_while(|line| !line.trim().starts_with("// ==/UserScript=="))
+            .collect::<Vec<&str>>()
+            .join("\n");
+        for cap in METADATA_KEY_VALUE_REGEX.captures_iter(&meta_block) {
+            let data = Data::new(cap["key"].trim(), cap["value"].trim());
+            metadata.push(data);
+        }
+
+        Self {
+            script: script.to_string(),
+            name: metadata.get("name"),
+            description: metadata.get("description"),
+            version: metadata.get("version"),
+            author: metadata.get("author"),
+            match_patterns: metadata.find_all("match"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_metadata() {
+        let script = r#"
+// ==UserScript==
+// @author       You
+// @name title
+// ==/UserScript==
+"#;
+        let user_script = UserScript::parse(script);
+        assert_eq!(user_script.author, Some("You".to_string()));
+        assert_eq!(user_script.name, Some("title".to_string()));
+    }
+}
