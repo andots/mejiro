9ad44c768c41fb7193c3ee0e8d9b68231b209ac2
andots <18536+andots@users.noreply.github.com>
2025-03-15 00:43:15 +0900

    fix(user-scripts): error handling and use fs::read_to_string

--- crates/user-scripts/src/lib.rs
+++ crates/user-scripts/src/lib.rs
@@ -4,7 +4,7 @@ mod utils;
 
 pub use error::Error;
 
-use std::{fs, io::Read, sync::Mutex};
+use std::{fs, sync::Mutex};
 use tauri::Manager;
 
 use parus_common::{constants::EXTERNAL_WEBVIEW_LABEL, AppHandlePathExt};
@@ -15,26 +15,22 @@ use utils::list_userscripts;
 const PLUGIN_NAME: &str = "user-scripts";
 
 trait AppHandleExt {
-    fn load_user_scripts(&self) -> Vec<UserScript>;
+    fn load_user_scripts(&self) -> Result<Vec<UserScript>, Error>;
     fn eval_user_scripts<T: tauri::Runtime>(&self, webview: &tauri::Webview<T>);
 }
 
 impl<R: tauri::Runtime> AppHandleExt for tauri::AppHandle<R> {
-    fn load_user_scripts(&self) -> Vec<UserScript> {
+    fn load_user_scripts(&self) -> Result<Vec<UserScript>, Error> {
         let mut scripts = vec![];
         let dir = self.get_userscripts_dir();
-        if let Ok(paths) = list_userscripts(&dir) {
-            for path in paths {
-                if let Ok(mut file) = fs::File::open(path) {
-                    let mut script = String::new();
-                    if file.read_to_string(&mut script).is_ok() {
-                        let user_script = UserScript::parse(&script);
-                        scripts.push(user_script);
-                    }
-                }
+        let paths = list_userscripts(&dir)?;
+        for path in paths {
+            if let Ok(script) = fs::read_to_string(path) {
+                let user_script = UserScript::parse(&script);
+                scripts.push(user_script);
             }
         }
-        scripts
+        Ok(scripts)
     }
 
     fn eval_user_scripts<T: tauri::Runtime>(&self, webview: &tauri::Webview<T>) {
@@ -53,19 +49,24 @@ pub fn init<R: tauri::Runtime>() -> tauri::plugin::TauriPlugin<R> {
     tauri::plugin::Builder::new(PLUGIN_NAME)
         .invoke_handler(tauri::generate_handler![])
         .setup(|app, _api| {
-            let scripts = app.load_user_scripts();
-            app.manage(Mutex::new(scripts));
+            match app.load_user_scripts() {
+                Ok(scripts) => {
+                    app.manage(Mutex::new(scripts));
+                }
+                Err(e) => log::error!("UserScript error: {}", e.to_string()),
+            }
             Ok(())
         })
         .on_page_load(|webview, payload| {
             if webview.label() == EXTERNAL_WEBVIEW_LABEL {
                 match payload.event() {
                     tauri::webview::PageLoadEvent::Started => {
+                        log::debug!("Started: {}", payload.url().as_str());
                         let handle = webview.app_handle();
                         handle.eval_user_scripts(webview);
                     }
                     tauri::webview::PageLoadEvent::Finished => {
-                        //
+                        log::debug!("Finished: {}", payload.url().as_str());
                     }
                 }
             }
